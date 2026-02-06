90cf93918d68009a861632375b13b094a7a52465
Jyzdcs <ky.claudant@gmail.com>
2026-02-06 12:50:04 +0100

    Add AMateria implementation with constructors, destructors, and methods for type management and usage. This includes copy constructor and assignment operator for proper memory handling.

---
+++ Module04/ex03/AMateria.cpp
@@ -0,0 +1,28 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const &type) : type(type) {
+	std::cout << "[AMateria] constructor called\n";
+};
+
+AMateria::AMateria(const AMateria& src) : type(src.type) {
+	std::cout << "[AMateria] copy constructor called\n";
+};
+
+AMateria& AMateria::operator=(const AMateria& src) {
+	if (this == &src) return *this;
+
+	this->type = src.type;
+	return *this;
+}
+
+AMateria::~AMateria() {
+	std::cout << "[AMateria] destructor called\n";
+}
+
+std::string const&	AMateria::getType() {
+	return type;
+};
+
+void			AMateria::use(ICharacter& target) {
+	std::cout << "Actual AMateria has a undefined type\n";
+};
