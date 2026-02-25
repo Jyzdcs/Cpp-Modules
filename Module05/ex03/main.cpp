a8cc40cca9b519928b8602d5afbf3269fd45fb21
Jyzdcs <ky.claudant@gmail.com>
2026-02-25 12:32:35 +0100

    Update main function to include additional form types: RobotomyRequestForm, PresidentialPardonForm, and an unknown form. Enhance error handling for form signing and execution. Clean up commented-out code and ensure proper resource management with deletions.

--- Module05/ex03/main.cpp
+++ Module05/ex03/main.cpp
@@ -2,8 +2,9 @@
 
 int main() {
 	AForm *shrubbery;
-	// AForm *robotomy;
-	// AForm *presidential;
+	AForm *robotomy;
+	AForm *presidential;
+	AForm *unknow;
 
 	try {
 		Bureaucrat kyks("pika", 1);
@@ -12,11 +13,47 @@ int main() {
 		shrubbery = test.makeForm("ShrubberyCreationForm", "test");
 		kyks.signForm(shrubbery);
 		kyks.executeForm(*shrubbery);
-		
+
+	}	catch (std::exception& err) {
+		std::cout << err.what();
+	}
+	std::cout << std::endl;
+	try {
+		Bureaucrat kyks("pika", 1);
+		Intern test;
+
+		robotomy = test.makeForm("RobotomyRequestForm", "test");
+		kyks.signForm(robotomy);
+		kyks.executeForm(*robotomy);
+
+	}	catch (std::exception& err) {
+		std::cout << err.what();
+	}
+	std::cout << std::endl;
+	try {
+		Bureaucrat kyks("pika", 1);
+		Intern test;
+
+		presidential = test.makeForm("PresidentialPardonForm", "test");
+		kyks.signForm(presidential);
+		kyks.executeForm(*presidential);
+
+	}	catch (std::exception& err) {
+		std::cout << err.what();
+	}
+	std::cout << std::endl;
+	try {
+		Bureaucrat kyks("pika", 1);
+		Intern test;
+
+		unknow = test.makeForm("unknow", "test");
+		kyks.signForm(unknow);
+		kyks.executeForm(*unknow);
+
 	}	catch (std::exception& err) {
 		std::cout << err.what();
 	}
 	delete shrubbery;
-	// delete robotomy;
-	// delete presidential;
+	delete robotomy;
+	delete presidential;
 }
