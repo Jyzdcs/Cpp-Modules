bea779314e48c424e6f1065d8dd22668e009c817
Jyzdcs <ky.claudant@gmail.com>
2026-03-05 12:23:14 +0100

    Refactor iter function to simplify template parameters and update main function to demonstrate usage with integer array. Comment out unused character manipulation function.

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -1,8 +1,8 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename A, typename L, typename F>
-void	iter(A adr, L const len, F *fct) {
+template <typename A, typename F>
+void	iter(A* adr, int const len, void (*fct)(F)) {
 	for (int i = 0; i < len; i++) {
 		fct(adr[i]);
 	};
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -5,11 +5,21 @@ void	multiply(int &n) {
 	n *= n;
 }
 
+// void	multiply(const char &c) {
+// 	std::cout << static_cast<char>(c + 1) << std::endl;
+// }
+
+void	addition(char& n) {
+	n = n + 1;
+}
+
 int main() {
+	// char arr[6] = {'a', 'b', 'c', 'd', 'e', 'f'};
 	int arr[6] = {0, 1, 2, 3, 4, 5};
 	int const len = 6;
 
-	iter(arr, len, &multiply);
+	// iter<char*, const int, void (const char &)>(arr, len, &multiply);
+	iter(arr, len, multiply);
 	std::cout << arr[0] << std::endl;
 	std::cout << arr[1] << std::endl;
 	std::cout << arr[2] << std::endl;
