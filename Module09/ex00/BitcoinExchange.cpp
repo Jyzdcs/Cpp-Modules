3ca1d19a9be8a8f5b402d770191fe8226ad6510b
Jyzdcs <ky.claudant@gmail.com>
2026-03-25 22:51:37 +0100

    Add BitcoinExchange class implementation with methods for validating input, calculating Bitcoin values, and handling file operations. Include main function for execution and a Makefile for compilation. Add sample data CSV for testing.

---
+++ Module09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,149 @@
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {
+	std::cout << "[BitcoinExchange] ctor called" << std::endl;
+};
+
+BitcoinExchange::~BitcoinExchange() {
+	std::cout << "[BitcoinExchange] dtor called" << std::endl;
+};
+
+bool	BitcoinExchange::isValidNumber(std::string str) {
+	int	virguleCount = 0;
+	for (size_t i = 0; i < str.size(); i++) {
+		if (i != 0 and str[i] == '.' and virguleCount == 0)	virguleCount++;
+		else if (!std::isdigit(str[i])) {
+			return false;
+		}
+	}
+	if (0 < atoi(str.c_str()) and atoi(str.c_str()) < 1000)
+		return true;
+	return false;
+};
+
+bool	BitcoinExchange::isOnlyDigit(std::string str) {
+	for (size_t i = 0; i < str.size(); i++) {
+		if (!std::isdigit(str[i]))	return false;
+	}
+	return true;
+};
+
+bool	BitcoinExchange::isValidDateFormat(std::string str) {
+	std::vector<std::string>	date;
+
+	size_t	pos = str.find("-");
+	while (pos != std::string::npos) {
+		date.push_back(str.substr(0, pos));
+		str.erase(0, pos + 1);
+		pos = str.find("-");
+	};
+	date.push_back(str);
+	if (date.size() != 3)	return false;
+	for (size_t	i = 0; i < date.size(); i++) {
+		if (!isOnlyDigit(date[i]))	return false;
+	};
+	if (atoi(date[0].c_str()) < 2009)	return false;
+	if (atoi(date[1].c_str()) < 1)	return false;
+	if (atoi(date[2].c_str()) < 1)	return false;
+	return true;
+};
+
+bool	BitcoinExchange::isValidExtension(std::string fileName) {
+	if (fileName.substr(fileName.find_last_of(".") + 1) == ".csv") {
+		return true;
+	}
+	return false;
+};
+
+int		BitcoinExchange::getDateValue(std::string date) {
+	std::vector<std::string>	dataCsv;
+	std::string								value;
+	dataCsv.swap(fileHandler._fileContent);
+
+	fileHandler.extractFileContent("./data.csv");
+	for (size_t i = 1; i < dataCsv.size(); i++) {
+		if (dataCsv[i].find(date) != std::string::npos) {
+			value.append(dataCsv[i].substr(dataCsv[i].find(",") + 1));
+			return atoi(value.c_str());
+		};
+	}
+	return 0;
+};
+
+void BitcoinExchange::calculBtc() {
+	int	value;
+	int	result;
+	
+	_lines.swap(fileHandler._fileContent);
+	if (_lines[0] != "date | value")	{
+		std::cout << "Error: invalid first line" << std::endl;
+	};
+	for (size_t i = 1; i < _lines.size(); i++) {
+		if (_lines[i].find(" | ") == std::string::npos)	{
+			std::cout << "Error: invalid input missing | " << std::endl;
+		} else {
+			std::vector<std::string>	linePart;
+			std::string	date;
+			std::string	multiplier;
+			date.append(_lines[i].substr(0, _lines[i].find(" | ")));
+			_lines[i].erase(0, _lines[i].find(" | ") + 3);
+			multiplier.append(_lines[i]);
+			if (!isValidNumber(multiplier)) {
+				std::cout << "Error: invalid number value = " << multiplier << std::endl;
+			} else {
+				if (!isValidDateFormat(date)) {
+					std::cout << "Error: invalid date format = " << date << std::endl;
+				} else {
+					// std::cout << "Success: valid date format = " << date << std::endl;
+					value = getDateValue(date);
+					// std::cout << "value of date '" << date << "' = " << value << std::endl;
+					result = value * atoi(multiplier.c_str());
+					std::cout << date << " => " << multiplier << " = " << result << std::endl;
+				}
+			}
+		}
+	}
+};
+
+void	BitcoinExchange::printInput() {
+	for (std::vector<std::string>::iterator it = _lines.begin(); it != _lines.end(); ++it) {
+		std::cout << *it << std::endl; 
+	};
+};
+
+BitcoinExchange::FileHandler::FileHandler() {
+	std::cout << "[FileHandler] ctor called" << std::endl;
+};
+
+BitcoinExchange::FileHandler::~FileHandler() {
+	std::cout << "[FileHandler] dtor called" << std::endl;
+};
+
+void	BitcoinExchange::FileHandler::extractFileContent(std::string path) {
+	std::ifstream file(path.c_str());
+	if (!file.is_open()) {
+		throw FileExtractFailed();
+	}
+
+	std::string line;
+
+	while (std::getline(file, line)) {
+		_fileContent.push_back(line);
+	}
+	
+	file.close();
+}
+
+void	BitcoinExchange::FileHandler::printFileContent() {
+	for (std::vector<std::string>::iterator it = _fileContent.begin(); it != _fileContent.end(); ++it) {
+		std::cout << *it << std::endl; 
+	};
+};
+
+const char *BitcoinExchange::FileHandler::FileExtractFailed::what() const throw() {
+	return "Error: Failed to extract the file content";
+};
+
+const char *BitcoinExchange::FileHandler::InvalidFileStructure::what() const throw() {
+	return "ParsingError: Invalid file structure";
+};
---
+++ Module09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,56 @@
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+// class	FileHandler;
+
+class BitcoinExchange {
+	private:
+		std::vector<std::string>	_lines;
+		bool	isValidNumber(std::string str);
+		bool	isValidDateFormat(std::string date);
+		bool	isOnlyDigit(std::string str);
+
+	public:
+		BitcoinExchange();
+		~BitcoinExchange();
+
+		bool	isValidExtension(std::string fileName);
+
+		int		getDateValue(std::string date);
+
+		void	calculBtc();
+		void	printInput();
+
+		class	FileHandler {
+			private:
+				std::string _outputFileName;
+				std::string	_occurence;
+				std::string _replaceStr;
+				std::string _outputFileContent;
+			
+			public:
+				FileHandler();
+				~FileHandler();
+				
+				std::vector<std::string> _fileContent;
+
+				void	extractFileContent(std::string path);
+				void	printFileContent();
+
+				class FileExtractFailed : public std::exception {
+					const char *what() const throw();
+				};
+				class InvalidFileStructure : public std::exception {
+					const char *what() const throw();
+				};
+		};
+		FileHandler								fileHandler;
+};
+
+#endif
