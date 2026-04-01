8737b48d69cf102182381c733002a21ae34fdba6
Jyzdcs <ky.claudant@gmail.com>
2026-04-01 13:00:01 +0200

    Remove commented-out code and clean up main function in BitcoinExchange implementation for improved readability.

--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -4,24 +4,12 @@
 
 int main(int ac, char **av) {
 	if (ac != 2)	return 1;
-	// parser le fichier en input
 	BitcoinExchange bitcoin;
 
-	// std::string str = "salut, ca va tlmd ?";
-	// std::string sub = str.substr(str.find(","));
-
-	// std::cout << sub << std::endl;
-
 	bitcoin.fileHandler.extractFileContent(av[1]);
-	// bitcoin.fileHandler.printFileContent();
 	try {
 		bitcoin.calculBtc();
 	} catch (std::exception& err) {
 		std::cout << err.what() <<std::endl;
 	}
-		// verifier si le fichier contient bien 2 colonnes au debut data et value
-		// verifier si chaques lignes n'a pas d'informations en trop
-	// set toutes les variables private
-	// appeler les methodes utilitaire pour creer le resultat final
-	// print le resultat final
 };
